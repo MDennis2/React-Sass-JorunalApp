2514b5a2b12e96064ded2fe6f8d5bfd4155b40b1
Dennis <dennis.mihaita11@gmail.com>
2021-03-28 15:15:35 +0200

    error

--- src/types/types.js
+++ src/types/types.js
@@ -10,5 +10,13 @@ export const types = {
     uiRemoveError: '[UI] Remove Error',
 
     uiStartLoading: '[UI] Start loading',
-    uiFinishLoading: '[UI] Finish loading'
+    uiFinishLoading: '[UI] Finish loading',
+
+    notesAddNew: '[Notes] New note',
+    notesActive: '[Notes] Set active note',
+    notesLoad: '[Notes] Load notes',
+    notesUpdated: '[Notes] Upgrade note',
+    notesFileUrl: '[Notes] Upgrade image url',
+    notesDelete: '[Notes] Delete note',
+    notesLogoutCleaning:'[Notes] Logout Cleaning'
 }
